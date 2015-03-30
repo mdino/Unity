405ad94e431c294bbffbe23800b821e8da9f53d4
Dino <dino.musliu@gmail.com>
2015-03-30 22:12:07 +0200

    Create touch2d.cs

---
+++ touch2d.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class touch2d : MonoBehaviour {
+
+	public float movementSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.touchCount == 1 ) {
+			transform.position -= Vector3.left * movementSpeed * Time.deltaTime;
+				}
+
+		else if (Application.platform == RuntimePlatform.Android){
+				if (Input.GetKeyDown(KeyCode.Escape)) 
+					Application.Quit();
+			}
+
+
+		}
+
+}
+
