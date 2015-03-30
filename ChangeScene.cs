709e96e99357234df2bd28114ab83faabb1ab62e
Dino <dino.musliu@gmail.com>
2015-03-30 22:10:08 +0200

    Create ChangeScene.cs

---
+++ ChangeScene.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+
+
+public class ChangeSceneScore : MonoBehaviour {
+
+	public void ChangeToScene (){
+		if (Score.highscore > 19) {
+						Application.LoadLevel (Random.Range (3, 10));
+				} 
+		else 
+						Application.LoadLevel ("level1");
+		}
+
+
+
+	/*public void ChangeToScene (string sceneToChangeTo){
+		Application.LoadLevel (sceneToChangeTo);
+
+
+}*/
+}
